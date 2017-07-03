6c2aee6ad034079624bfef5c74eb029ea3710ae6
windspinks <windspinks@gmail.com>
2017-07-03 09:48:11 -0400

    db mustache

--- app.js
+++ app.js
@@ -39,16 +39,17 @@ app.use(morgan("dev"));
 const pg = require('pg');
 
 app.get('/db', function (req, res) {
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM test_table', function(err, result) {
+  pg.connect(process.env.DATABASE, function(err, client, done) {
+    client.query('SELECT * FROM users', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
-       { res.render('pages/db', {results: result.rows} ); }
+       { res.render('db', {results: result.rows} ); }
     });
   });
 });
+
 app.use(routes);
 
 app.set('port', (process.env.PORT || 5000));
