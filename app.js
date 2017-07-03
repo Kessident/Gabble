2ef5bb2f364deadd6481dbf435940445bfc3df0b
windspinks <windspinks@gmail.com>
2017-07-03 09:44:56 -0400

    pg databse things

--- app.js
+++ app.js
@@ -33,8 +33,22 @@ app.use(session({
   saveUninitialized: false
 }));
 
+//Logger
 app.use(morgan("dev"));
 
+const pg = require('pg');
+
+app.get('/db', function (req, res) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { res.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 app.use(routes);
 
 app.set('port', (process.env.PORT || 5000));
