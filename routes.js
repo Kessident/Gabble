7f2a17edea687138c5ad281c803254f4147a664b
windspinks <windspinks@gmail.com>
2017-07-03 10:14:35 -0400

    removed pg

--- routes.js
+++ routes.js
@@ -3,20 +3,6 @@ const router = express.Router();
 const models = require("./models");
 let errorMsg = [];
 
-const pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
 const checkLogin = function(req, res, next) {
   if (req.session.username) {
     next();
