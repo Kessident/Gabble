ef03dd990040b021eaac28892356ce7bb3d01302
windspinks <windspinks@gmail.com>
2017-07-03 16:56:17 -0400

    pg command

--- app.js
+++ app.js
@@ -38,6 +38,20 @@ app.use(session({
 //logger
 app.use(morgan("dev"));
 
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
+
 app.use(routes);
 
 app.listen(app.get('port'), function() {
