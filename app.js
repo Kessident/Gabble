05acc3bcdf4b868009e0c79e9e092889423f73e0
windspinks <windspinks@gmail.com>
2017-07-03 10:01:34 -0400

    pg moved to routes

--- app.js
+++ app.js
@@ -36,20 +36,6 @@ app.use(session({
 //Logger
 app.use(morgan("dev"));
 
-const pg = require('pg');
-
-pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
-
 app.use(routes);
 
 app.set('port', (process.env.PORT || 5000));
